c53e2bf2e551a79cc7ba76b08905e19f3bc74e0b
Jean <jean.begaint@gmail.com>
2014-01-03 12:46:40 +0100

    rename current i3 workspace script

--- bin/mpc_dmenu.sh
+++ bin/mpc_dmenu.sh
@@ -4,7 +4,8 @@ COMMANDS="
 toggle\n
 next\n
 prev\n
-playlists
+playlists\n
+artist
 "
 
 function load_playlist {
@@ -14,16 +15,27 @@ function load_playlist {
 	mpc -q play
 }
 
+function load_artist {
+	mpc -q clear
+	mpc -q find artist "$1" | mpc add
+	mpc -q play
+}
+
 function dmenu_wrapper {
 	dmenu -i -sb '#4A6787' -p "$1:"
 }
 
-CMD=$(echo -e $COMMANDS | dmenu_wrapper "MPC command")
+CMD=$(echo -e $COMMANDS | dmenu_wrapper "action")
 
 if [[ "$CMD" == " playlists" ]]
 then
-	PLAYLIST=$(mpc lsplaylists | dmenu_wrapper "MPD playlist:" )
+	PLAYLIST=$(mpc lsplaylists | dmenu_wrapper "playlist" )
 	load_playlist "$PLAYLIST"
+
+elif [[ "$CMD" == " artist" ]]
+then
+	artist=$(mpc list artist | dmenu_wrapper "artist")
+	load_artist "$artist"
 else
 	mpc -q $CMD;
 fi
